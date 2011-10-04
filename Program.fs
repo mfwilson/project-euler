5dc2e9e079d10a2e330c2c6134764fdf37456f11
mwilson <mwilson@localhost>
2011-10-04 14:13:36 -0500

    Problem 6

--- Program.fs
+++ Program.fs
@@ -8,7 +8,7 @@ open ProjectEulerConsole
 //printfn "1221 %b" (Shared.Palindromes.IsValid "1221")
 
 
-let result = ProjectEuler.Problem9
+let result = ProjectEuler.Problem6
 printfn "%d" result
 
 Console.ReadLine() |> ignore
